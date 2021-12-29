7f5143e827248d320483dc02749c59935466a101
Krzysztof <krzubrycki@gmail.com>
2021-12-29 17:40:53 +0100

    extracted Person.class

---
+++ src/com/zubrycky/lesson3/Person.java
@@ -0,0 +1,31 @@
+package com.zubrycky.lesson3;
+
+class Person {
+    private String name;
+    private int yearOfBirth;
+
+    public Person(String name, int yearOfBirth) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Person) {
+            Person other = (Person) o;
+            if (other.yearOfBirth == this.yearOfBirth) {
+                if (other.name == null && this.name == null) return true;
+                if (other.name != null && this.name != null) {
+                    if (other.name.equals(this.name)) return true;
+                }
+                return false;
+            }
+            return false;
+        } else return false;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + yearOfBirth;
+    }
+}
---
+++ src/com/zubrycky/lesson4/Lesson4.java
@@ -0,0 +1,8 @@
+package com.zubrycky.lesson4;
+
+public class Lesson4 implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
