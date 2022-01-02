6d9dd470752d02defd656c3d1bf2c60b6b56377b
Krzysztof <krzubrycki@gmail.com>
2022-01-02 19:18:40 +0100

    default test

--- src/com/zubrycky/Main.java
+++ src/com/zubrycky/Main.java
@@ -1,10 +1,10 @@
 package com.zubrycky;
 
-import com.zubrycky.lesson7.Lesson7;
+import com.zubrycky.lesson6.Lesson6;
 
 public class Main {
 
     public static void main(String[] args) {
-        new Lesson7().run();
+        new Lesson6().run();
     }
 }
--- src/com/zubrycky/lesson7/c/DefaultTest3.java
+++ src/com/zubrycky/lesson7/c/DefaultTest3.java
@@ -1,2 +1,10 @@
-package com.zubrycky.lesson7.c;public class DefaultTest3 {
+package com.zubrycky.lesson7.c;
+
+import com.zubrycky.lesson7.a.DefaultTest;
+
+public class DefaultTest3 extends DefaultTest {
+    void test(){
+        //Nawet jeżeli dziedziczymy po tej klasie, nie ma dostępu do pola default
+        //print(name);
+    }
 }
