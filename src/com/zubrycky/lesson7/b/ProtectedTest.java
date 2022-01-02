8129f167ebaa3cff70309787a75e2f9537b19b6f
Krzysztof <krzubrycki@gmail.com>
2022-01-02 19:14:40 +0100

    protected test

--- src/com/zubrycky/lesson7/b/ProtectedTest.java
+++ src/com/zubrycky/lesson7/b/ProtectedTest.java
@@ -3,9 +3,13 @@ package com.zubrycky.lesson7.b;
 import com.zubrycky.lesson7.a.DefaultTest;
 
 public class ProtectedTest {
+
+    protected String id = "1231";
+
     void test(){
         DefaultTest defaultTest = new DefaultTest();
         //Nie możemy odczytać pola name ponieważ jesteśmy  w innym package
         //defaultTest.name;
     }
+
 }
