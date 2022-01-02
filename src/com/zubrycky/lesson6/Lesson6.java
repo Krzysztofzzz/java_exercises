cf44334dac280b73435907373559bdf8154a229d
Krzysztof <krzubrycki@gmail.com>
2022-01-02 18:30:15 +0100

    added print creditCards

--- src/com/zubrycky/lesson6/Lesson6.java
+++ src/com/zubrycky/lesson6/Lesson6.java
@@ -1,4 +1,30 @@
 package com.zubrycky.lesson6;
 
-public class Lesson6 {
+import com.zubrycky.lesson5.CreditCard;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static com.zubrycky.utils.Utils.print;
+
+public class Lesson6 implements Runnable {
+    @Override
+    public void run() {
+        List<CreditCard> creditCards = new ArrayList<>();
+        addNewCard(creditCards);
+        printCreditCards(creditCards);
+    }
+
+    private void addNewCard(List<CreditCard> creditCardList) {
+        Random random = new Random();
+        CreditCard creditCard1 = new CreditCard(random.nextLong());
+        creditCardList.add(creditCard1);
+    }
+
+    private void printCreditCards(List<CreditCard> creditCardList) {
+        for (CreditCard card : creditCardList) {
+            print(card);
+        }
+    }
 }
