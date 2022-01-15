82e921ae56637aa32113c877f8a3db84c456a55d
Krystian Zubrycki <xxrcd@eg.dk>
2022-01-15 12:52:29 +0100

    Prepare Lesson16

--- src/com/zubrycky/lesson/lesson16_streams/Lesson16Stream.java
+++ src/com/zubrycky/lesson/lesson16_streams/Lesson16Stream.java
@@ -1,9 +1,28 @@
 package com.zubrycky.lesson.lesson16_streams;
 
+import com.zubrycky.excercise.excercise1.Animal;
+import com.zubrycky.excercise.excercise1.Cat;
+import com.zubrycky.excercise.excercise1.Fish;
+import com.zubrycky.excercise.excercise1.Spider;
+
+import java.util.List;
+
 public class Lesson16Stream implements Runnable {
 
     @Override
     public void run() {
-        //
+        final List<Animal> animals = List.of(
+                new Fish("nemo"),
+                new Cat("freja"),
+                new Spider()
+        );
+    }
+
+    private double calculateAverageLegsCount(List<Animal> animals) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    private List<String> getPetsNames(List<Animal> animals) {
+        throw new UnsupportedOperationException("not implemented");
     }
 }
