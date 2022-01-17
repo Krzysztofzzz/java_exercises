e456048217d52cab35f0e12a117000b662bf8939
Krzysztof <krzubrycki@gmail.com>
2022-01-17 19:07:42 +0100

    vararg test

--- src/com/zubrycky/lesson/lesson18_vararg_labels/Lesson18.java
+++ src/com/zubrycky/lesson/lesson18_vararg_labels/Lesson18.java
@@ -1,8 +1,26 @@
 package com.zubrycky.lesson.lesson18_vararg_labels;
 
+import static com.zubrycky.utils.Utils.print;
+
 public class Lesson18 implements Runnable {
     @Override
     public void run() {
-        //
+        test("ala");
+        test("ala", "ma");
+        test("ala", "ma", "kota");
+        test();
+        test2(1,"fiut");
+        test2(1,"fiut","kdsjnfs","dfssd");
     }
+
+    public void test(String... strings) {
+        for (int i = 0; i < strings.length; i++) {
+            print(strings[i]);
+        }
+
+    }
+    void test2(Integer i, String label, String... strings){
+
+    }
+
 }
