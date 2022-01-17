56e2bfbae0b287b290b29df8ecd088b4eb8f1eb7
Krzysztof <krzubrycki@gmail.com>
2022-01-17 19:39:49 +0100

    continue and labels practice

--- src/com/zubrycky/lesson/lesson18_vararg_labels/Lesson18.java
+++ src/com/zubrycky/lesson/lesson18_vararg_labels/Lesson18.java
@@ -5,12 +5,16 @@ import static com.zubrycky.utils.Utils.print;
 public class Lesson18 implements Runnable {
     @Override
     public void run() {
-        test("ala");
+/*        test("ala");
         test("ala", "ma");
         test("ala", "ma", "kota");
         test();
-        test2(1,"fiut");
-        test2(1,"fiut","kdsjnfs","dfssd");
+        test2(1, "fiut");
+        test2(1, "fiut", "kdsjnfs", "dfssd");*/
+
+        //loopTest();
+
+        loopTest2();
     }
 
     public void test(String... strings) {
@@ -19,8 +23,39 @@ public class Lesson18 implements Runnable {
         }
 
     }
-    void test2(Integer i, String label, String... strings){
 
+    void test2(Integer i, String label, String... strings) {
+
+    }
+
+    void loopTest() {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                continue;
+            }
+            for (int j = 0; j < 10; j++) {
+                if (j == 1) {
+                    continue;
+                }
+                print("i = " + i + ", j = " + j);
+            }
+        }
+    }
+
+    void loopTest2() {
+        outerLoop:
+        for (int i = 0; i < 6; i++) {
+            //innerLoop:
+            for (int j = 0; j < 6; j++) {
+                if (i != 0 && j != 0 && i % 2 == 0 && j % 2 == 0) {
+
+                    continue outerLoop;
+                }
+                print("i = " + i + ", j = " + j);
+            }
+
+
+        }
     }
 
 }
