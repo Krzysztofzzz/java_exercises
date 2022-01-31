3d5162770f8d1c16fbbd36d40408ea2228586f2c
Krzysztof <krzubrycki@gmail.com>
2022-01-31 20:45:18 +0100

    Bad usage of third SOLID principle

--- test/com/zubrycky/lesson/lesson20_SOLID/RectangleTest.java
+++ test/com/zubrycky/lesson/lesson20_SOLID/RectangleTest.java
@@ -9,8 +9,9 @@ public class RectangleTest {
     @Test
     public void whenSettingRectangleHeightItsWidthDoesNotChange() {
         double rectangleInitialWidth = 10;
-        Rectangle rectangle = new Rectangle(rectangleInitialWidth, 20);
+        Rectangle rectangle = new SquareBadExample(rectangleInitialWidth);
         rectangle.setHeight(50);
+
 
         Assert.assertEquals(rectangleInitialWidth, rectangle.getWidth(),0);
 
