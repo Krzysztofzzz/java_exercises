34530500c6fc3bdea600724d5b5249cad59cecca
Krzysztof <krzubrycki@gmail.com>
2022-02-28 17:26:59 +0100

    completed excercise and tests

--- src/com/zubrycky/Main.java
+++ src/com/zubrycky/Main.java
@@ -1,10 +1,9 @@
 package com.zubrycky;
 
-import com.zubrycky.excercise.ecxercise4_palindrome.Excercise4Palindrome;
-
 public class Main {
 
     public static void main(String[] args) {
-        new Excercise4Palindrome().run();
+
+
     }
 }
--- src/com/zubrycky/excercise/excercise5_armstrong_number/ArmstrongNumber.java
+++ src/com/zubrycky/excercise/excercise5_armstrong_number/ArmstrongNumber.java
@@ -1,4 +1,22 @@
 package com.zubrycky.excercise.excercise5_armstrong_number;
 
 public class ArmstrongNumber {
+
+    public boolean isArmstrongNumber(int number) {
+
+        int changingOriginalNumber = number;
+        int length = Integer.toString(number).length();
+        int tempNumber;
+        int armstrong = 0;
+
+        for (int i = 0; i<length; i++){
+            tempNumber = changingOriginalNumber%10;
+            armstrong += tempNumber*tempNumber*tempNumber;
+            changingOriginalNumber = changingOriginalNumber/10;
+        }
+
+        if (armstrong==number){
+            return true;
+        }else return false;
+    }
 }
