e4368cb93ff93451dc8476249d216114a32ae051
Krzysztof <krzubrycki@gmail.com>
2022-03-09 18:44:47 +0100

    wip

--- src/com/zubrycky/excercise/excercise9_recurison/PermutationRecursion.java
+++ src/com/zubrycky/excercise/excercise9_recurison/PermutationRecursion.java
@@ -2,37 +2,21 @@ package com.zubrycky.excercise.excercise9_recurison;
 
 public class PermutationRecursion {
 
-
-    // Utility function to swap two characters in a character array
-    private void swap(char[] chars, int i, int j) {
-        char temp = chars[i];
-        chars[i] = chars[j];
-        chars[j] = temp;
-    }
-
-    // Recursive function to generate all permutations of a string
-    private void permutations(char[] chars, int currentIndex) {
-        if (currentIndex == chars.length - 1) {
-            System.out.println(String.valueOf(chars));
-        }
-
-        for (int i = currentIndex; i < chars.length; i++) {
-            swap(chars, currentIndex, i);
-            permutations(chars, currentIndex + 1);
-            swap(chars, currentIndex, i);
-        }
-    }
-
-    public void permutate(String str) {
-
-        // base case
-        if (str == null || str.length() == 0) {
+    public void permutate(String input) {
+        if (input == null || input.isEmpty()) {
             return;
         }
+        for (int i = 0; i < input.length(); i++) {
+            String letter = input.substring(i,i+1);
 
-        permutations(str.toCharArray(), 0);
+            for (int j = 1; j<input.length(); j++){
+                String nextLetter = input.substring(j,j+1);
+                input.substring()
+            }
+
+
+
+        }
     }
-
-
 }
 
